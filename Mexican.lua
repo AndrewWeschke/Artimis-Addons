214e39df005599522b11bedb45b15a9925e3c276
Andrew Weschke <AndrewWeschke@gmail.com>
2015-12-22 14:19:06 -0500

    Create Mexican.lua

---
+++ Mexican.lua
@@ -0,0 +1,14 @@
+TEAM_SECURITY = AddExtraTeam("Mexican", {
+color = Color( 184, 138, 0, ),
+model = "models/player/guerilla.mdl",
+description = [[Base with other Mexicans
+and take down
+the Border Patrol!]],
+weapons = {"weapon_crowbar"},
+command = "mexican",
+max = 7
+salary = 20,
+admin = 0,
+vote = false,
+hasLicense = false,
+})
